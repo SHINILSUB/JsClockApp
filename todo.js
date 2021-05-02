d655e7d9e9c1b2935af011bcab80ca5b27310d50
SHINILSUB <deathsizes@hotmail.com>
2021-05-02 22:42:52 +0900

    todoPtwo

--- todo.js
+++ todo.js
@@ -3,16 +3,31 @@ const toDoForm = document.querySelector('.js-toDoForm'),
     toDoList = document.querySelector('.js-toDoList')
 
 const TODOS_LS = "toDos"
+const toDos = []
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos))
+}
+
 
 function paintToDo(text) {
     const li = document.createElement("li")
     const delBtn = document.createElement("button")
-    delBtn.innerHTML = "❌"
     const span = document.createElement("span")
+    const newId = toDos.length + 1;
+    delBtn.innerHTML = "❌"
     span.innerText = text
     li.appendChild(span)
     li.appendChild(delBtn)
+    li.id = newId
     toDoList.appendChild(li)
+    const toDoObj = {
+        text: text,
+        id: newId
+
+    }
+    toDos.push(toDoObj)
+    saveToDos()
 
 }
 
@@ -22,13 +37,17 @@ function handleSubmit(event) {
     const currentValue = toDoInput.value
     paintToDo(currentValue)
     toDoInput.value = ""
-
-
 }
 
+
+
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS)
-    if (toDos !== null) {
+    const loadedToDos = localStorage.getItem(TODOS_LS)
+    if (loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos)
+        parsedToDos.forEach(function(toDo) {
+            paintToDo(toDo.text)
+        });
 
     }
 }
